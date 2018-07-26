83e3ec503d8da5685ced1cf2c5f293f7907498cb
Francisco Igor <franciscoigor@gmail.com>
2018-07-26 08:00:04 -0700

    change css style to react style part 1

--- components/Card.js
+++ components/Card.js
@@ -4,24 +4,35 @@ class Card extends React.Component {
     this.state = {
       
     };
-    this.styles = { 
+    this.headerStyle = { 
+      padding: 10,
+      fontSize: "1.2rem",
+      fontWeight: "bold",
+      height: "3rem",
+      textAlign: "center",
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "column",
+      borderRadius: "var(--radius1) var(--radius1) 0px 0px",
+      backgroundColor: "rgba(64,64,64,0.2)"
     }
-    this.classNames = ['card', this.props.className].join(" ")
+    this.contentStyle={
+      textAlign: "center",
+      padding: "8px 4px"
+    }
     if (props.image){
-      this.styles = {
-        backgroundImage: `url(${props.image})`,
-        backgroundPosition: "center center",
-        backgroundSize: "cover"
-      }  
+        this.headerStyle["backgroundImage"] = `url(${props.image})`;
+        this.headerStyle["backgroundPosition"] = "center center";
+        this.headerStyle["backgroundSize"] = "cover";
     }
   }
   render() {
     return (
-      <div className={this.classNames} >
-        <div className="card-header" style={this.styles} >
+      <div className="card" >
+        <div className="card-header" style={this.headerStyle} >
             <div className="card-header-title">{this.props.title}</div>
         </div>
-        <div className="card-content" >
+        <div className="card-content" style={this.contentStyle} >
             {this.props.children}
         </div>
       </div>
